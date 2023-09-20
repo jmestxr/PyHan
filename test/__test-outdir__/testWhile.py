bb86e99d96c424de3f45557a58e41012ec1a1ffd
jmestxr <tan_xing_rong@hotmail.com>
2023-09-21 00:29:49 +0800

    Add test scripts

---
+++ test/__test-outdir__/testWhile.py
@@ -0,0 +1,4 @@
+x=0
+while x<5:
+    print("yay")
+    x=x+1
---
+++ test/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Directories
+SCRIPT_DIR=$(dirname "$0")
+PYHAN_COMPILER="${SCRIPT_DIR}/../compiler.py"
+TEST_FILES_DIR="${SCRIPT_DIR}/test-files"
+TEST_OUTDIR="${SCRIPT_DIR}/__test-outdir__"
+
+# Iterate over test files
+for test_file_path in "${TEST_FILES_DIR}"/*.pyhan; do
+    test_file=$(basename "$test_file_path")
+    echo "Running test: ${test_file}"
+    python "${PYHAN_COMPILER}" "${test_file_path}" "${TEST_OUTDIR}/${test_file%.*}.py"
+done
